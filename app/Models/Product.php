217fef73cb81ab82a4000ad327e87368d0942ac5
Eurynomosek <wasikc4@wp.pl>
2021-11-18 20:59:57 +0100

    #18 Lista produktów dla niezalogowanego użytkownika

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -15,6 +15,7 @@ class Product extends Model
      * @var string[]
      */
     protected $fillable = [
+        'image_path',
         'name',
         'description',
         'amount',
--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -8,7 +8,7 @@
                 <div class="card-header">Edycja produktu</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('products.update', $product->id) }}">
+                    <form method="POST" action="{{ route('products.update', $product->id) }}" enctype="multipart/form-data">
                         @csrf
 
                         <div class="form-group row">
@@ -67,7 +67,20 @@
                             </div>
                         </div>
 
+                        <div class="form-group row">
+                            <label for="image" class="col-md-4 col-form-label text-md-right">Grafika</label>
 
+                            <div class="col-md-6">
+                                <input id="image" type="file" class="form-control" name="image">
+                            </div>
+                        </div>
+
+                        <div class="form-group row justify-content-center">
+
+                            <div class="col-md-6">
+                                <img src="{{ asset('storage/' . $product->image_path) }}" width="240" height="240">
+                            </div>
+                        </div>
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
