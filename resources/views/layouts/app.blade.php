d4ebbf735ca2b3e9d8963a48b334e90468b02267
Eurynomosek <wasikc4@wp.pl>
2021-11-26 09:53:05 +0100

    x

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -58,7 +58,7 @@
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
 
-                                    <a class="dropdown-item" href="/users/list">Użytkownicy</a>
+                                    <a class="dropdown-item" href="{{ route('users.index') }}">Użytkownicy</a>
                                     <a class="dropdown-item" href="{{ route('products.index') }}">Produkty</a>
 
                                     <a class="dropdown-item" href="{{ route('logout') }}"
--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ Route::get('/products/edit/{product}', [ProductController::class, 'edit'])->name
 Route::post('/products/{product}', [ProductController::class, 'update'])->name('products.update')->middleware('auth');
 Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.destroy')->middleware('auth');
 
-Route::get('/users/list', [UserController::class, 'index'])->middleware('auth');
+Route::get('/users/list', [UserController::class, 'index'])->name('users.index')->middleware('auth');
 Route::delete('/users/{user}', [UserController::class, 'destroy'])->middleware('auth');
 
 Auth::routes();
